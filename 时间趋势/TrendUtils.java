20c5a0f6fff42642fb4d622dfbc1462f000d1604
hixingchen <2781003217@qq.com>
2024-01-04 11:29:22 +0800

    hixingchen

--- 时间趋势/TrendUtils.java
+++ 时间趋势/TrendUtils.java
@@ -54,19 +54,17 @@ public class TrendUtils {
     public static Map<String, List> recentTimeTrend(List<Map> maps,Integer nums,String type){
         return recentTimeTrend(maps,nums,"date","value",type);
     }
-    //key默认date,value默认为value,type默认小时，nums默认近12小时
-    public static Map<String, List> recentTimeTrend(List<Map> maps){
-        return recentTimeTrend(maps,12,"date","value","hour");
-    }
+
     /**
      * 计算当前时间趋势图
      * @param maps sql查询的时间数据（月/日/时）
      * @param key 存储（月/日/时）时间对应字段
      * @param value 存储（月/日/时）值对应字段
      * @param type 时间类型（month/day/hour）
+     * @param intercept 是否截取时间到当前时间
      * @return
      */
-    public static Map<String,List> currentTimeTrend(List<Map> maps,String key,String value,String type){
+    public static Map<String,List> currentTimeTrend(List<Map> maps,String key,String value,String type,Boolean intercept){
         int timeBegin = 0;
         int timeEnd = 23;
         int maxTime = 0;
@@ -77,7 +75,7 @@ public class TrendUtils {
         List<Integer> dateList = new ArrayList<>();//趋势图的x轴
         Map<Integer,String> target = new HashMap<>();
         for (int i = 0; i < maps.size(); i++) {
-                target.put(Integer.valueOf(maps.get(i).get(key).toString()),maps.get(i).get(value).toString());
+            target.put(Integer.valueOf(maps.get(i).get(key).toString()),maps.get(i).get(value).toString());
         }
         switch (type){
             case "hour":
@@ -101,7 +99,9 @@ public class TrendUtils {
         for (int i = timeBegin; i <= timeEnd; i++) {
             dateList.add(i);
         }
-
+        if (!intercept){
+            maxTime = timeEnd;
+        }
         for (int i = timeBegin; i <= maxTime; i++) {
             valueList.add(target.getOrDefault(i,"0"));
         }
@@ -110,12 +110,13 @@ public class TrendUtils {
         return result;
     }
 
-    //key默认date,value默认value
+    //key默认date,value默认value,intercept默认true
     public static Map<String,List> currentTimeTrend(List<Map> maps,String type){
-        return currentTimeTrend(maps,"date","value",type);
+        return currentTimeTrend(maps,"date","value",type,true);
     }
-    //key默认date,value默认value,type默认hour
-    public static Map<String,List> currentTimeTrend(List<Map> maps){
-        return currentTimeTrend(maps,"date","value","hour");
+
+    //key默认date,value默认value
+    public static Map<String,List> currentTimeTrend(List<Map> maps,String type,Boolean intercept){
+        return currentTimeTrend(maps,"date","value",type,intercept);
     }
 }
